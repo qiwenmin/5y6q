809eac4ab8631063b92b80db75519471e4ff2400
Qi Wenmin <qiwenmin@gmail.com>
2018-08-03 18:55:47 +0800

    加入部署脚本。

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Deploy to 1st netlify site:"
+netlifyctl deploy
+
+echo "Deploy to 5y6q netlify site:"
+netlifyctl -C netlify-5y6q.toml deploy
+
+echo "Deploy to 5y6q.surge.sh:"
+surge build 5y6q.surge.sh
